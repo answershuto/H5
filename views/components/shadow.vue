d07235ebef26d2c38e7cbc6af02e49721a099a90
answershuto <905307789@qq.com>
2016-12-29 21:21:53 +0800

    加载组件

---
+++ views/components/shadow.vue
@@ -0,0 +1,59 @@
+<template>
+ 	<div>
+		<div class="mask opacity">
+			<div class="div-ProgressBar">
+				<div class="div-ProgressBar-conter">
+					<mu-circular-progress :size="180" color="red" strokeWidth="10"></mu-circular-progress>
+					<div class="div-loading">正在加载...</div>
+				</div>
+			</div>
+		</div>
+ 	</div>
+</template>
+
+<script>
+
+	export default {
+		components: {
+		 	
+		}
+	}
+</script>
+
+<style scoped>
+	.div-loading{
+		color: red;
+		margin-top: -100px;
+		margin-left: 50px;
+		font-size: 120%;
+	}
+
+	.mask{
+		height:100%; 
+		width:100%; 
+		position:fixed; 
+		_position:absolute; 
+		top:0; 
+		z-index:1000; 
+	} 
+
+	.opacity{ 
+		opacity:0.3; 
+		filter: 
+		alpha(opacity=30); 
+		background-color:#000; 
+	} 
+
+	.div-ProgressBar{
+		width: 200px;
+		margin: 0px auto;
+	}
+
+	.div-ProgressBar-conter{
+		position: absolute; 
+		width: 200px; 
+		top: 50%;
+		margin-top: -150px;
+		height: 300px;
+	}
+</style>
