c5dcb5aa1f97fef6e085119664963d563bf5ec08
answershuto <905307789@qq.com>
2017-01-01 22:45:23 +0800

    增加主页导航栏

---
+++ views/components/Navigation.vue
@@ -0,0 +1,39 @@
+<template>
+ 	<div class="">
+		<mu-paper>
+  <mu-bottom-nav :value="bottomNav" @change="handleChange">
+    <mu-bottom-nav-item value="home" title="主页" icon="home"/>
+    <mu-bottom-nav-item value="grade" title="我的作品" icon="grade"/>
+    <mu-bottom-nav-item value="build" title="设置" icon="build"/>
+    <mu-bottom-nav-item value="power" title="退出" icon="clear"/>
+  </mu-bottom-nav>
+</mu-paper>
+ 	</div>
+</template>
+
+<script>
+
+	export default {
+		components: {
+		 	
+		},
+		data(){
+			return {
+				bottomNav: 'home'
+		    }
+		},
+		methods: {
+			handleChange (val) {
+				this.bottomNav = val
+			}
+		},
+	}
+</script>
+
+<style scoped>
+	.container{
+		width: 100%;
+		height: 80px;
+		background-color: #eeff41;
+	}
+</style>
