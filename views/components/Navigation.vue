e52db4c208533d6343ded0fae81a6c3fbe848454
answershuto <905307789@qq.com>
2017-01-02 20:43:49 +0800

    不同导航处理函数

--- views/components/Navigation.vue
+++ views/components/Navigation.vue
@@ -1,18 +1,18 @@
 <template>
  	<div class="">
 		<mu-paper>
-  <mu-bottom-nav :value="bottomNav" @change="handleChange">
-    <mu-bottom-nav-item value="home" title="主页" icon="home"/>
-    <mu-bottom-nav-item value="grade" title="我的作品" icon="grade"/>
-    <mu-bottom-nav-item value="build" title="设置" icon="build"/>
-    <mu-bottom-nav-item value="power" title="退出" icon="clear"/>
-  </mu-bottom-nav>
+			<mu-bottom-nav :value="bottomNav" @change="handleChange">
+			<mu-bottom-nav-item value="home" title="主页" icon="home"/>
+			<mu-bottom-nav-item value="grade" title="我的作品" icon="grade"/>
+			<mu-bottom-nav-item value="build" title="设置" icon="build"/>
+			<mu-bottom-nav-item value="power" title="退出" icon="clear"/>
+		</mu-bottom-nav>
 </mu-paper>
  	</div>
 </template>
 
 <script>
-
+	let Cookies = require('js-cookie');
 	export default {
 		components: {
 		 	
@@ -25,7 +25,40 @@
 		methods: {
 			handleChange (val) {
 				this.bottomNav = val
-			}
+
+				switch(val){
+					case 'home':
+						break;
+					case 'grade':
+						break;
+					case 'build':
+						break;
+					case 'power':
+						/*退出*/
+						console.log(Cookies.get('H5-isRememberPassword'))
+						fetch('/H5/rpc',
+							{
+								method:'POST',
+								headers:{ 
+						 			'Accept': 'application/json', 
+						 			'Content-Type': 'application/json'
+								},
+								credentials: 'include',
+								body: JSON.stringify({
+									method: 'logout',
+									params: {
+										
+									},
+								})
+							}
+						)
+						.then(response => response.json())
+						.then(d => {console.log(d)})
+						break;
+					default:
+						alert('无此标签')
+				}
+			},
 		},
 	}
 </script>
