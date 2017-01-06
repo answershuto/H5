10e39ee4f098efc44f6750cb6b5b8a25cad0553e
answershuto <905307789@qq.com>
2017-01-06 22:06:12 +0800

    没有作品的显示页

--- views/layouts/Work.vue
+++ views/layouts/Work.vue
@@ -1,6 +1,9 @@
 <template>
  	<div class="container">
- 		Work
+ 		<div class="prompt">
+ 			您还没有作品，赶紧创建一个吧.
+ 			<mu-flat-button label="创建作品" class="create-button" @click="createWork" primary/>
+ 		</div>
  	</div>
 </template>
 
@@ -9,12 +12,31 @@
 	export default {
 		components: {
 			
-		}
+		},
+		methods: {
+			createWork(){
+				
+			},
+		},
 	}
 </script>
 
 <style scoped>
 	.container{
-		
+		width: 300px;
+		margin: 0px auto;
+	}
+
+	.prompt{
+		position: absolute; 
+		width: 300px; 
+		top: 50%;
+		height: 300px;
+		font-size: 150%;
+		text-align: center;
+	}
+
+	.create-button{
+		margin-top: 30px;
 	}
 </style>
