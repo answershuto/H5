ad91f379a7ea1f9c57c36edadd0d0f8e653d5332
answershuto <905307789@qq.com>
2017-01-10 22:59:05 +0800

    增加musicDialog

---
+++ views/layouts/musicDialog.vue
@@ -0,0 +1,52 @@
+<template>
+	<mu-dialog :open="isOpen" title="" >	
+		<div class="music-left">
+			<mu-list @change="handleChange">
+				<mu-list-item title="我的音乐" value="myMusic" >
+					<mu-icon slot="left" value="audiotrack"/>
+				</mu-list-item>
+				<mu-list-item title="网络乐库" value="networkMusic" >
+					<mu-icon slot="left" value="audiotrack"/>
+				</mu-list-item>
+			</mu-list>
+		</div>
+		<div class="music-right">
+			
+		</div>
+	</mu-dialog>
+</template>
+
+<script>
+
+	export default {
+		components: {
+
+		},
+		methods:{
+			handleChange(val){
+				switch(val){
+					case 'myMusic':
+						break;
+					case 'networkMusic':
+						break;
+				}
+			}
+		},
+		computed:{
+			isOpen(){
+				return true;
+			}
+		}
+	}
+</script>
+
+<style scoped>
+	.music-left{
+		float: left;
+		width: 160px;
+	}
+
+	.music-right{
+		float: left;
+	}
+</style>
--- views/pages/Design.vue
+++ views/pages/Design.vue
@@ -2,16 +2,19 @@
  	<div>
  		<PageList></PageList>
  		<MobilePhone></MobilePhone>
+ 		<musicDialog></musicDialog>
  	</div>
 </template>
 
 <script>
 	import PageList from '../layouts/PageList.vue'
 	import MobilePhone from '../layouts/MobilePhone.vue'
+	import musicDialog from '../layouts/musicDialog.vue'
 	export default {
 		components: {
 			PageList,
 			MobilePhone,
+			musicDialog,
 		},
 		computed: {
 			workName(){
