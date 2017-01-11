8371c579515a0d4c60c8d53157fb43b4da46f9c9
answershuto <905307789@qq.com>
2017-01-11 22:07:50 +0800

    实现网络乐库展示并用audio标签播放

--- views/layouts/musicDialog.vue
+++ views/layouts/musicDialog.vue
@@ -11,7 +11,21 @@
 			</mu-list>
 		</div>
 		<div class="music-right">
-			
+			<div v-show="isMyMusic">
+				
+			</div>
+			<div v-show="isNetMusic">
+				<div>
+					<audio id="myAudio" controls="controls" >
+						<source  type="audio/mp3" />
+					</audio>
+				</div>
+				<div>
+					<span v-for="item in musicItems">
+						<mu-flat-button :label="item.title" @click="handleClickMusic(item.url)" class="" icon="audiotrack" primary/>
+					</span>
+				</div>
+			</div>
 		</div>
 	</mu-dialog>
 </template>
@@ -22,19 +36,86 @@
 		components: {
 
 		},
+		data(){
+			return {
+				currentPage: 'myMusic',
+				musicSrc: "",
+				musicItems: [
+					{
+						title: 'Above and Beyond',
+						url: 'Audio Machine - Above and Beyond.mp3',
+					},
+					{
+						title: 'Age of Dragons',
+						url: 'Audio Machine - Age of Dragons.mp3',
+					},
+					{
+						title: 'Impera',
+						url: 'Audio Machine - Impera.mp3',
+					},
+					{
+						title: 'Lachrimae',
+						url: 'Audio Machine - Lachrimae.mp3',
+					},
+					{
+						title: 'Piano Beat',
+						url: 'BeaTsGOy - Piano Beat.mp3',
+					},
+					{
+						title: 'Legacy',
+						url: 'Brian Tyler - Legacy',
+					},
+					{
+						title: '你再也读不出我任何欲望',
+						url: 'Cicada - 你再也读不出我任何欲望.mp3',
+					},
+					{
+						title: '最后 仍在一起',
+						url: 'Cicada - 最后 仍在一起.mp3',
+					},
+					{
+						title: 'Requiem For A Tower Mvt. III',
+						url: 'Corner Stone Cues - Requiem For A Tower Mvt. III.mp3',
+					},
+					{
+						title: 'Our Flame Shall Endure',
+						url: 'Epic Score - Our Flame Shall Endure.mp3',
+					},
+					{
+						title: 'Aphelion V2',
+						url: 'Fired Earth Music - Aphelion V2.mp3',
+					},
+					{
+						title: 'Passion of Victory',
+						url: 'Future World Music - Passion of Victory.mp3',
+					},
+					{
+						title: 'End Credits',
+						url: 'Hans Zimmer - End Credits.mp3',
+					},
+				]
+			}
+		},
 		methods:{
 			handleChange(val){
-				switch(val){
-					case 'myMusic':
-						break;
-					case 'networkMusic':
-						break;
-				}
-			}
+				this.currentPage = val;
+			},
+			handleClickMusic(url){
+				let audio = document.getElementById('myAudio');
+				audio.pause();
+				audio.src = '/music/'+url;
+				audio.play();
+			},
 		},
 		computed:{
 			isOpen(){
 				return true;
+			},
+			isMyMusic(){
+				return (this.currentPage === 'myMusic')
+			},
+			isNetMusic(){
+				return (this.currentPage === 'networkMusic')
 			}
 		}
 	}
