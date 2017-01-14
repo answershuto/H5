32f471ac1801faae2c45afd655fd2856cec89c86
answershuto <905307789@qq.com>
2017-01-14 22:22:04 +0800

    查询用户所有音乐后端实现

--- app/controllers/h5.server.rpc.js
+++ app/controllers/h5.server.rpc.js
@@ -1,8 +1,55 @@
 'use strict';
 
+let mongoose = require('mongoose');
+let UserMusics = mongoose.model('UserMusics');
+
 module.exports = {
+	/**
+	 * 用户登出
+	 *
+	 * 用户登出接口，释放资源
+	 *
+	 * @param    req 
+	 * @param    res 
+	 * @param    next 
+	 * @returns  void
+	 *
+	 * @date     2017-1-14
+	 * @author   Cao Yang
+	 */
 	logout(req,res,next){
 		delete req.session.user;
 		res.json({result: true});
+	},
+
+	/**
+	 * 获取所有音乐
+	 *
+	 * 获取用户上传的所有音乐的列表
+	 *
+	 * @param    req 
+	 * @param    res 
+	 * @param    next 
+	 * @returns  void
+	 *
+	 * @date     2017-1-14
+	 * @author   Cao Yang
+	 */
+	getAllUserMusics(req, res, next){
+		UserMusics.find({userName: req.session.user.userName}, null,{}, (err, result) => {
+			if (err) {
+
+			}
+			else{
+				let params = [];
+				result.forEach((item, index) => {
+					params.push({
+						id: item._id,
+						musicName: item.musicName,
+					})
+				})
+				res.json({result: true, params});
+			}
+		})
 	},
 }
