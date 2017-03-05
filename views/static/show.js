2f78a0e2c0459f257e0aea21775cb2935f5310c5
answershuto <905307789@qq.com>
2017-03-05 21:27:44 +0800

    用消息队列实现纪录滑动动作，延迟动画执行，优化动画保证动画如丝般顺滑

--- views/static/show.js
+++ views/static/show.js
@@ -1,5 +1,55 @@
 (function(){
 
+	/*消息处理中心*/
+	function newsCenter(){
+		this.callbacks = {};
+		this.news = [];
+		var isAnimation = false;/*动画标识位，在动画过程中不能继续出发上拉下滑功能*/
+
+		setInterval(function(){
+			if (this.news.length && !isAnimation) {
+				var newObj = this.news.shift();
+				isAnimation = true;
+				this.callbacks[newObj.type].forEach(function(f){
+					typeof f === 'function' && f.call(newObj.that);
+					setTimeout(function(){
+						isAnimation = false;
+					}, AnimationTime())
+				})
+			}
+		}.bind(this), 100)
+	};
+
+	/*注册一个消息回调*/
+	newsCenter.prototype.attach = function(type, func){
+		(this.callbacks[type] === undefined) && (this.callbacks[type] = []);
+		this.callbacks[type].push(func);
+	}
+
+	/*注销一个消息回调*/
+	newsCenter.prototype.detach = function(type, func){
+		if (!this.callbacks[type]) return;
+
+		var index = this.callbacks[type].indexOf(func);
+		if (index > -1) {
+			this.callbacks[type].splice(index, 1);
+		}
+	}
+
+	/*触发事件*/
+	newsCenter.prototype.fire = function(type, that){
+		if (this.callbacks[type] && this.news.length < 1) {
+			/*事件中心最多纪录两条信息，再多直接丢弃（这里包括一条已经在执行的，已经推出消息队列）*/
+			this.news.push({'type':type, 'that': that});
+		}
+	}
+
+	/*新建事件处理中心并注册回调函数*/
+	var newsCenterObj = new newsCenter();
+	newsCenterObj.attach('swipeUp',up);
+	newsCenterObj.attach('swipeDown',down);
+
+	/*动画执行时间*/
 	function AnimationTime(){
 		return 1000;
 	}
@@ -46,30 +96,29 @@
 		}
 	})
 
-	var isAnimation = false;/*动画标识位，在动画过程中不能继续出发上拉下滑功能*/
 	var myTouch = util.toucher(document.getElementById('contain'));
 	myTouch.on('swipeUp',function(e){
-	    if ((elePage < (pages.length - 1)) && !isAnimation) {
-	    	isAnimation = true;
-			showPage($(pages[elePage + 1]), 'up');
-			hidePage($(pages[elePage]), 'up');
-			elePage++;
-			setTimeout(function(){
-				isAnimation = false;
-			}, AnimationTime());
-		}
+	    newsCenterObj.fire('swipeUp');
 	})
 
 	myTouch.on('swipeDown',function(e){
-	    if ((elePage > 0) && !isAnimation) {
-	    	isAnimation = true;
+	   newsCenterObj.fire('swipeDown');
+	})
+
+	function up(){
+		if ((elePage < (pages.length - 1))) {
+			showPage($(pages[elePage + 1]), 'up');
+			hidePage($(pages[elePage]), 'up');
+			elePage++;
+		}
+	}
+
+	function down(){
+		 if ((elePage > 0)) {
 			showPage($(pages[elePage - 1]), 'down');
 			hidePage($(pages[elePage]), 'down');
 			elePage--;
-			setTimeout(function(){
-				isAnimation = false;
-			}, AnimationTime());
 		}
-	})
+	}
 
 })();
